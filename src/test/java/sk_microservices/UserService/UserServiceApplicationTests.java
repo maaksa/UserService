85f0a7d2c52c31f284602ccd962c18b8f7ea385a
maaksa <milosmilos@ymail.com>
2020-12-10 18:30:18 +0100

    init

---
+++ src/test/java/sk_microservices/UserService/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package sk_microservices.UserService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
